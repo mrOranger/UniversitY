dbe87e744d2dc6d147107c1cc5b3380a041caa2a
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-01 10:38:03 +0100

    Complete initialization of the project

---
+++ university-student/src/main/java/it/university/student/UniversityStudentApplication.java
@@ -0,0 +1,13 @@
+package it.university.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityStudentApplication.class, args);
+	}
+
+}
