caf92f0d355fcb70b76d7fbd81bf3332b121ab44
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-02 11:01:19 +0100

    Add reference between the entities

--- university-student/src/main/java/it/university/student/entity/Department.java
+++ university-student/src/main/java/it/university/student/entity/Department.java
@@ -1,11 +1,30 @@
 package it.university.student.entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class Department {
 	
+	@Id @Column(name = "name") 
+	@NotNull(message = "{NotNull.Department.Name.Validation}")
+	@Size(min = 5, max = 15, message = "{Size.Department.Id.Validation}")
 	private String name;
 	
+	@ManyToOne
+	@JoinColumn(name = "address", referencedColumnName = "id")
 	private Address address;
+	
+	@OneToOne(mappedBy = "director", cascade = CascadeType.ALL, orphanRemoval = true)
 	private Professor director;
+	
+	@ManyToOne
+	@JoinColumn(name = "department", referencedColumnName = "name")
 	private Faculty faculty; 
 
 }
--- university-student/src/main/java/it/university/student/entity/Professor.java
+++ university-student/src/main/java/it/university/student/entity/Professor.java
@@ -1,25 +1,19 @@
 package it.university.student.entity;
 
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
