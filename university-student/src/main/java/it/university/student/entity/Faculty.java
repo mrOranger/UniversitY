7dd9f3b9ced5c47377225ca9e571950595748c7a
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-02 11:07:03 +0100

    Creted entity Faculty

--- university-student/src/main/java/it/university/student/entity/Faculty.java
+++ university-student/src/main/java/it/university/student/entity/Faculty.java
@@ -1,8 +1,35 @@
 package it.university.student.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 public class Faculty {
 	
+	@Id @Column(name = "name")
+	@NotNull(message = "{NotNull.Faculty.Name.Validation}")
+	@Size(min = 5, max = 15, message = "{Size.Faculty.Name.Validation}")
 	private String name;
+	
+	@OneToOne
+	@PrimaryKeyJoinColumn
+	@JsonIgnore
 	private Professor director;
+	
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "faculty", orphanRemoval = true)
+	@JsonManagedReference
+	private Set<Department> departments = new HashSet<>();
 
 }
