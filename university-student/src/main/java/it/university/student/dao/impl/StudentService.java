48194c4cc2c21354b0fc7b5acbdb0a394812ef77
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-02 12:05:19 +0100

    Complete definition of PagingRepository and Service DAO

---
+++ university-student/src/main/java/it/university/student/dao/impl/StudentService.java
@@ -0,0 +1,56 @@
+package it.university.student.dao.impl;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import it.university.student.dao.StudentDao;
+import it.university.student.entity.Student;
+import it.university.student.repository.StudentRepository;
+
+public class StudentService implements StudentDao{
+	
+	@Autowired private StudentRepository repository;
+
+	@Override
+	public List<Student> findAll() {
+		return StreamSupport.stream(this.repository.findAll().spliterator(), false)
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public Student findById(String id) {
+		return this.repository.findById(id).orElse(null);
+	}
+
+	@Override
+	public List<Student> findAllByDate(LocalDate start, LocalDate end) {
+		return this.repository
+				.findByDateOfBirthBetweenStartAndEnd(Date.valueOf(start), Date.valueOf(end));
+	}
+
+	@Override
+	public List<Student> findAllBySex(char sex) {
+		return this.repository.findBySex(sex);
+	}
+
+	@Override
+	public List<Student> findAllByBachelorDegree() {
+		return this.repository.findByBachelorDegreeNotNull();
+	}
+
+	@Override
+	public List<Student> findAllByDiplomaGrade(byte diplomaGrade) {
+		return this.repository.findByDiplomaGrade(diplomaGrade);
+	}
+
+	@Override
+	public List<Student> findAllByBachelorGrade(byte bachelorGrade) {
+		return this.repository.findByBachelorDegree(bachelorGrade);
+	}
+
+}
---
+++ university-student/src/main/java/it/university/student/repository/StudentRepository.java
@@ -0,0 +1,18 @@
+package it.university.student.repository;
+
+import java.sql.Date;
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import it.university.student.entity.Student;
+
+public interface StudentRepository extends PagingAndSortingRepository<Student, String>{
+	
+	public abstract List<Student> findByDateOfBirthBetweenStartAndEnd(Date start, Date end);
+	public abstract List<Student> findBySex(char sex);
+	public abstract List<Student> findByBachelorDegreeNotNull();
+	public abstract List<Student> findByDiplomaGrade(byte diplomaGrade);
+	public abstract List<Student> findByBachelorDegree(byte bachelorGrade);
+	
+}
