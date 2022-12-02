28d5db9b92de1cd175a6f0d43e580cea142f6cb1
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-02 12:30:32 +0100

    Missing Lombok and Entity references

--- university-student/src/main/java/it/university/student/entity/Faculty.java
+++ university-student/src/main/java/it/university/student/entity/Faculty.java
@@ -6,17 +6,25 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Table(name = "Faculties")
+@Data @AllArgsConstructor @NoArgsConstructor
 public class Faculty implements Serializable {
 	
 	private static final long serialVersionUID = -7069931654707277600L;
