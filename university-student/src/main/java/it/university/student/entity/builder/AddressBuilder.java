412033709471c5e55d75a79d537dd58d4e2b7e52
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-03 16:32:57 +0100

    Created Builder design pattern to semplify creation of objects

---
+++ university-student/src/main/java/it/university/student/entity/builder/AddressBuilder.java
@@ -0,0 +1,75 @@
+package it.university.student.entity.builder;
+
+import it.university.student.entity.Address;
+import it.university.student.entity.Department;
+import it.university.student.entity.Professor;
+import it.university.student.entity.Student;
+
+public class AddressBuilder implements Builder<Address>{
+	
+	private Address address;
+	
+	public AddressBuilder() {
+		this.address = new Address();
+	}
+	
+	public AddressBuilder(Address address) {
+		this.address = address;
+	}
+	
+	public AddressBuilder setId(int id) {
+		this.address.setId(id);
+		return this;
+	}
+	
+	public AddressBuilder setStreet(String street) {
+		this.address.setStreet(street);
+		return this;
+	}
+	
+	public AddressBuilder setNumber(int number) {
+		this.address.setNumber(number);
+		return this;
+	}
+
+	public AddressBuilder setCity(String city) {
+		this.address.setCity(city);
+		return this;
+	}
+	
+	public AddressBuilder setProvince(String province) {
+		this.address.setProvince(province);
+		return this;
+	}
+	
+	public AddressBuilder setRegion(String region) {
+		this.address.setRegion(region);
+		return this;
+	}
+	
+	public AddressBuilder setNation(String nation) {
+		this.address.setNation(nation);
+		return this;
+	}
+	
+	public AddressBuilder addStudent(Student student) {
+		this.address.getStudents().add(student);
+		return this;
+	}
+	
+	public AddressBuilder addProfessor(Professor professor) {
+		this.address.getProfessors().add(professor);
+		return this;
+	}
+	
+	public AddressBuilder addDepartment(Department department) {
+		this.address.getDepartments().add(department);
+		return this;
+	}
+	
+	@Override
+	public Address build() {
+		return address;
+	}
+
+}
---
+++ university-student/src/main/java/it/university/student/entity/builder/Builder.java
@@ -0,0 +1,7 @@
+package it.university.student.entity.builder;
+
+public interface Builder<T> {
+	
+	public abstract T build();
+
+}
