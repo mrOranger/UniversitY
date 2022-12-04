f1eb855de0db2329b76a5f026ff4484dcae04a53
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-04 21:53:48 +0100

    Complete configuration of test classes

--- university-student/src/test/java/it/university/student/controller/TestStudentControllerDelete.java
+++ university-student/src/test/java/it/university/student/controller/TestStudentControllerDelete.java
@@ -1,5 +1,31 @@
 package it.university.student.controller;
 
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import it.university.student.UniversityStudentApplication;
+
+@ContextConfiguration(classes = UniversityStudentApplication.class)
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
 public class TestStudentControllerDelete {
+	
+	private MockMvc mockMcv;
+	@Autowired private WebApplicationContext context;
+	
+	@BeforeEach
+	public void setup() throws JSONException, IOException {
+		this.mockMcv = MockMvcBuilders.webAppContextSetup(context).build();
+	}
 
 }
