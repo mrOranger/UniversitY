d2803d2550235e789de4e4dea072b16d091a06e3
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-05 17:41:34 +0100

    Injected StudentService dependency

--- university-student/src/main/java/it/university/student/controller/StudentController.java
+++ university-student/src/main/java/it/university/student/controller/StudentController.java
@@ -5,74 +5,78 @@ import java.util.List;
 
 import javax.websocket.server.PathParam;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.university.student.dao.impl.StudentService;
 import it.university.student.entity.Student;
 import lombok.SneakyThrows;
 
 @RestController @RequestMapping(path = "api/students/", produces = MediaType.APPLICATION_JSON_VALUE)
-public class StudentController {
+public final class StudentController {
+	
+	@Autowired private StudentService service;
 	
 	@RequestMapping(path = "find/") @SneakyThrows
-	public ResponseEntity<List<Student>> getStudents() {
+	public final ResponseEntity<List<Student>> getStudents() {
 		return null;
 	}
 	
 	@RequestMapping(path = "find/{id}") @SneakyThrows
-	public ResponseEntity<Student> getStudent(@PathParam("id") String id) {
+	public final ResponseEntity<Student> getStudent(@PathParam("id") String id) {
 		return null;
 	}
 	
 	@RequestMapping(path = "find/date/{start}/{end}") @SneakyThrows
-	public ResponseEntity<List<Student>> getStudentsByDateOfBirth(@PathParam("start") Date start, @PathParam("end") Date end) {
+	public final ResponseEntity<List<Student>> getStudentsByDateOfBirth(@PathParam("start") Date start, @PathParam("end") Date end) {
 		return null;
 	}
 	
 	@RequestMapping(path = "find/sex/{sex}") @SneakyThrows
-	public ResponseEntity<List<Student>> getStudentsBySex(@PathParam("sex") char sex) {
+	public final ResponseEntity<List<Student>> getStudentsBySex(@PathParam("sex") char sex) {
 		return null;
 	}
 	
 	@RequestMapping(path = "find/bachelor/has") @SneakyThrows
-	public ResponseEntity<List<Student>> getStudentsHavingBachelorDegreee() {
+	public final ResponseEntity<List<Student>> getStudentsHavingBachelorDegreee() {
 		return null;
 	}
 	
 	@RequestMapping(path = "find/bachelor/has/not") @SneakyThrows
-	public ResponseEntity<List<Student>> getStudentsNotHavingBachelorDegreee() {
+	public final ResponseEntity<List<Student>> getStudentsNotHavingBachelorDegreee() {
 		return null;
 	}
 	
 	@RequestMapping(path = "find/diploma/grade/{grade}") @SneakyThrows
-	public ResponseEntity<List<Student>> getStudentsByDiplomaGrade(@PathParam("grade") byte grade) {
+	public final ResponseEntity<List<Student>> getStudentsByDiplomaGrade(@PathParam("grade") byte grade) {
 		return null;
 	}
 	
 	@RequestMapping(path = "find/diploma/bachelor/grade/{grade}") @SneakyThrows
-	public ResponseEntity<List<Student>> getStudentsByBachelorGrade(@PathParam("grade") byte grade) {
+	public final ResponseEntity<List<Student>> getStudentsByBachelorGrade(@PathParam("grade") byte grade) {
 		return null;
 	}
 	
-	public ResponseEntity<List<Student>> getStudentsByCity(String city) {
+	public final ResponseEntity<List<Student>> getStudentsByCity(String city) {
 		return null;
 	}
 	
-	public ResponseEntity<List<Student>> getStudentsByProvince(String province) {
+	public final ResponseEntity<List<Student>> getStudentsByProvince(String province) {
 		return null;
 	}
 	
-	public ResponseEntity<List<Student>> getStudentsByRegion(String region) {
+	public final ResponseEntity<List<Student>> getStudentsByRegion(String region) {
 		return null;
 	}
 	
-	public ResponseEntity<List<Student>> getStudentsByNation(String nation) {
+	public final ResponseEntity<List<Student>> getStudentsByNation(String nation) {
 		return null;
 	}
 	
-	public ResponseEntity<List<Student>> getStudentsByAddress(int address) {
+	public final ResponseEntity<List<Student>> getStudentsByAddress(int address) {
 		return null;	
 	}
 }
