ebba7ade7bd70b10c721b1f8e6e7c58aa1de0c66
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-05 17:38:01 +0100

    Add exception handling and content type return

--- university-student/src/main/java/it/university/student/controller/StudentController.java
+++ university-student/src/main/java/it/university/student/controller/StudentController.java
@@ -5,51 +5,53 @@ import java.util.List;
 
 import javax.websocket.server.PathParam;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.university.student.entity.Student;
+import lombok.SneakyThrows;
 
-@RestController @RequestMapping(path = "api/students/")
+@RestController @RequestMapping(path = "api/students/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class StudentController {
 	
-	@RequestMapping(path = "find/")
+	@RequestMapping(path = "find/") @SneakyThrows
 	public ResponseEntity<List<Student>> getStudents() {
 		return null;
 	}
 	
-	@RequestMapping(path = "find/{id}")
+	@RequestMapping(path = "find/{id}") @SneakyThrows
 	public ResponseEntity<Student> getStudent(@PathParam("id") String id) {
 		return null;
 	}
 	
-	@RequestMapping(path = "find/date/{start}/{end}")
+	@RequestMapping(path = "find/date/{start}/{end}") @SneakyThrows
 	public ResponseEntity<List<Student>> getStudentsByDateOfBirth(@PathParam("start") Date start, @PathParam("end") Date end) {
 		return null;
 	}
 	
-	@RequestMapping(path = "find/sex/{sex}")
+	@RequestMapping(path = "find/sex/{sex}") @SneakyThrows
 	public ResponseEntity<List<Student>> getStudentsBySex(@PathParam("sex") char sex) {
 		return null;
 	}
 	
-	@RequestMapping(path = "find/bachelor/has")
+	@RequestMapping(path = "find/bachelor/has") @SneakyThrows
 	public ResponseEntity<List<Student>> getStudentsHavingBachelorDegreee() {
 		return null;
 	}
 	
-	@RequestMapping(path = "find/bachelor/has/not")
+	@RequestMapping(path = "find/bachelor/has/not") @SneakyThrows
 	public ResponseEntity<List<Student>> getStudentsNotHavingBachelorDegreee() {
 		return null;
 	}
 	
-	@RequestMapping(path = "find/diploma/grade/{grade}")
+	@RequestMapping(path = "find/diploma/grade/{grade}") @SneakyThrows
 	public ResponseEntity<List<Student>> getStudentsByDiplomaGrade(@PathParam("grade") byte grade) {
 		return null;
 	}
 	
-	@RequestMapping(path = "find/diploma/bachelor/grade/{grade}")
+	@RequestMapping(path = "find/diploma/bachelor/grade/{grade}") @SneakyThrows
 	public ResponseEntity<List<Student>> getStudentsByBachelorGrade(@PathParam("grade") byte grade) {
 		return null;
 	}
