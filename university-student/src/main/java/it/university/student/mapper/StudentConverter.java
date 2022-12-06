f89c95dd265615c17551442700f0b52234b85e28
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-06 10:16:19 +0100

    Add model mapper and converter for entities and DTO

---
+++ university-student/src/main/java/it/university/student/mapper/StudentConverter.java
@@ -0,0 +1,13 @@
+package it.university.student.mapper;
+
+import org.modelmapper.Converter;
+import org.modelmapper.spi.MappingContext;
+
+public class StudentConverter implements Converter<String, String>{
+
+	@Override
+	public String convert(MappingContext<String, String> context) {
+		return context.getSource() == null ? "" : context.getSource().trim();
+	}
+
+}
