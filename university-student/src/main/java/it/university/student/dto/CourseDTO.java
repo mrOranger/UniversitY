37dee450d93a1d5fd5ff0faef0155ed649969949
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-06 10:16:01 +0100

    Make 1 to 1 correspondence between fields

--- university-student/src/main/java/it/university/student/dto/CourseDTO.java
+++ university-student/src/main/java/it/university/student/dto/CourseDTO.java
@@ -1,7 +1,10 @@
 package it.university.student.dto;
 
 import java.sql.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+import it.university.student.entity.Professor;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +12,9 @@ import lombok.NoArgsConstructor;
 @Data @AllArgsConstructor @NoArgsConstructor
 public final class CourseDTO {
 	
+	private int id;
 	private String name;
-	private Date startDate;
-	private ProfessorDTO professor;
+	private Date date;
+	private Set<Professor> professors = new HashSet<>();
 
 }
