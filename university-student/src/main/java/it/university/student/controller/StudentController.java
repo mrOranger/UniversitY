7e4126f314bf97ffa5c41306d3463aeb9237de73
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-07 18:33:21 +0100

    Add new routes to controller

--- university-student/src/main/java/it/university/student/controller/StudentController.java
+++ university-student/src/main/java/it/university/student/controller/StudentController.java
@@ -1,27 +1,44 @@
 package it.university.student.controller;
 
 import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.university.student.dao.impl.StudentService;
 import it.university.student.dto.StudentDTO;
+import it.university.student.entity.Student;
+import it.university.student.exception.BindingException;
+import it.university.student.exception.DuplicateException;
 import it.university.student.exception.EmptyCollectionException;
 import it.university.student.exception.NotFoundException;
+import it.university.student.info.InfoMessage;
 import lombok.SneakyThrows;
+import lombok.extern.java.Log;
 
 @RestController @RequestMapping(path = "api/students/", produces = MediaType.APPLICATION_JSON_VALUE)
+@Log
 public final class StudentController {
 	
 	@Autowired private StudentService service;
+	@Autowired private ResourceBundleMessageSource errorMessage;
 	
 	@GetMapping(path = "find/") @SneakyThrows
 	public final ResponseEntity<List<StudentDTO>> getStudents() {
@@ -34,6 +51,7 @@ public final class StudentController {
 	
 	@GetMapping(path = "find/{id}") @SneakyThrows
 	public final ResponseEntity<StudentDTO> getStudent(@PathVariable("id") String id) {
+		log.info("Richiesto studente con id".concat(id));
 		final StudentDTO student = this.service.findById(id);
 		if(student == null) {
 			throw new NotFoundException();
@@ -129,5 +147,48 @@ public final class StudentController {
 			throw new EmptyCollectionException();
 		}
 		return new ResponseEntity<List<StudentDTO>>(students, HttpStatus.OK);
+	}
+	
+	@PostMapping(path = "post") @SneakyThrows
+	public final ResponseEntity<InfoMessage> postStudent(@Valid @RequestBody Student student, BindingResult bindingResult) {
+		log.info("Inserimento studente ".concat(student.toString()));
+		if(bindingResult.hasErrors()) {
+			log.info("Lo studente deve avere necessariamente una data di nascita!");
+			throw new BindingException("Lo studente deve avere necessariamente una data di nascita!");
+		}
+		final StudentDTO studentFound = this.service.findById(student.getId());
+		if(studentFound != null) {
+			log.info("Studente già presente nel database!");
+			throw new DuplicateException("Studente già presente nel database!");
+		}
+		
+		this.service.save(student);
+		
+		return new ResponseEntity<InfoMessage>(new InfoMessage(HttpStatus.CREATED.value(), LocalDate.now(), "Studente inserito con successo!"), HttpStatus.CREATED);
+	}
+	
+	@PutMapping(path = "put") @SneakyThrows
+	public final ResponseEntity<InfoMessage> putStudent(@Valid @RequestBody Student student, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			throw new BindingException(this.errorMessage.getMessage(bindingResult.getFieldError(), LocaleContextHolder.getLocale()));
+		}
+		final StudentDTO studentFound = this.service.findById(student.getId());
+		if(studentFound == null) {
+			throw new NotFoundException();
+		}
+		
+		this.service.save(student);
+		
+		return new ResponseEntity<InfoMessage>(new InfoMessage(HttpStatus.CREATED.value(), LocalDate.now(), "Studente modificato con successo!"), HttpStatus.CREATED);		
+	}
+
+	@DeleteMapping(path = "delete/{id}") @SneakyThrows
+	public final ResponseEntity<InfoMessage> deleteStudent(@PathVariable("id") String id) {
+		final StudentDTO student = this.service.findById(id);
+		if(student == null) {
+			throw new NotFoundException();
+		}
+		this.service.deleteStudent(id);
+		return new ResponseEntity<InfoMessage>(new InfoMessage(HttpStatus.ACCEPTED.value(), LocalDate.now(), "Studente eliminato con successo!"), HttpStatus.ACCEPTED);		
 	}
 }
