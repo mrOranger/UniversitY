49e5ba38bf58bdee3d5f25a1bc005dbf1aa4b64a
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-11 19:56:08 +0100

    Setup of test for select

--- university-department/src/test/java/it/university/department/repository/SelectTest.java
+++ university-department/src/test/java/it/university/department/repository/SelectTest.java
@@ -1,17 +1,91 @@
 package it.university.department.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.util.List;
+
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest @TestMethodOrder(OrderAnnotation.class) @Order(1)
 public class SelectTest {
 	
-	@Test @Order(1)
-	public void test() {
-		System.out.println("Test Select");
-	}
+	@Autowired private DepartmentService departmentService;
+	
+	@Autowired private DepartmentBuilder departmentBuilder;
+	@Autowired private AddressBuilder addressBuilder;
+	@Autowired private FacultyBuilder facultyBuilder;
 
+	@Test @Order(1)
+	public void testfindDepartments() {
+		final List<Department> departments = this.departmentService.findAll();
+		assertEquals(departments.size(), 8);
+	}
+	
+	@Test @Order(2)
+	public void testFindDepartmentByName() {
+		
+		final Address address = this.addressBuilder
+				.setStreet("Via Nazionale")
+				.setNumber(123)
+				.setCity("Milano")
+				.setProvince("Milano")
+				.setRegion("Lombardia")
+				.setNation("Italia")
+				.build();
+		
+		final Faculty faculty = this.facultyBuilder
+				.setName("Ingegneria")
+				.setAddress(address)
+				.build();
+		
+		final Department department = this.departmentService.findById("Ingegneria Informatica");
+		assertThat(department)
+			.extracting(Department::getName)
+			.isEqualTo("Ingegneria Informatica");
+		
+		assertThat(department)
+			.extracting(Department::getAddress)
+			.isEqualTo(address);
+		
+		assertThat(department)
+			.extracting(Department::getFaculty)
+			.isEqualTo(faculty);
+	}
+	
+	@Test @Order(3)
+	public void testFindDepartmentByNameFail() {
+		final Department department = this.departmentService.findById("Ingegneria Matematica");
+		assertNull(department);
+	}
+	
+	@Test @Order(4)
+	public void testFindDepartmentsByAddress() {
+		final List<Department> departments = this.departmentService.findAllByAddress(1);
+		assertEquals(departments.size(), 1);
+	}
+	
+	@Test @Order(5)
+	public void testFindDepartmentsByAddressFail() {
+		final List<Department> departments = this.departmentService.findAllByAddress(10);
+		assertEquals(departments.size(), 0);
+	}
+	
+	@Test @Order(6)
+	public void testFindDepartmentsByFaculty() {
+		final List<Department> departments = this.departmentService.findAllByFaculty("Ingegneria");
+		assertEquals(departments.size(), 3);
+	}
+	
+	@Test @Order(7)
+	public void testFindDepartmentsByFacultyFail() {
+		final List<Department> departments = this.departmentService.findAllByFaculty("Medicina");
+		assertEquals(departments.size(), 0);
+	}
 }
