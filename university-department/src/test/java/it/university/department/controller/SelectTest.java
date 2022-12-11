fe55f75383d1292192bc17519f1042747cde0feb
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-11 19:16:12 +0100

    Add test files and junit property file for class order

---
+++ university-department/src/test/java/it/university/department/controller/SelectTest.java
@@ -0,0 +1,11 @@
+package it.university.department.controller;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest @TestMethodOrder(OrderAnnotation.class) @Order(1)
+public class SelectTest {
+
+}
---
+++ university-department/src/test/java/it/university/department/repository/InsertTest.java
@@ -0,0 +1,11 @@
+package it.university.department.repository;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest @TestMethodOrder(OrderAnnotation.class) @Order(2)
+public class InsertTest {
+
+}
