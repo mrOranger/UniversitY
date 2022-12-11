5986a10b630badf8c97954928e4e206b5d62f318
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-11 18:51:27 +0100

    Initialized Department microservice

---
+++ university-department/src/main/java/it/university/department/UniversityDepartmentApplication.java
@@ -0,0 +1,13 @@
+package it.university.department;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityDepartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityDepartmentApplication.class, args);
+	}
+
+}
