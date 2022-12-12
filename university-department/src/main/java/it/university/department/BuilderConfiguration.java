54874369aa20dcb72ca4ae5e5a8461b87b3c889e
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-12 14:07:24 +0100

    Add builder method

--- university-department/src/main/java/it/university/department/BuilderConfiguration.java
+++ university-department/src/main/java/it/university/department/BuilderConfiguration.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration;
 
 import it.university.department.entity.Address;
 import it.university.department.entity.Faculty;
+import it.university.department.entity.Student;
 import it.university.department.entity.builder.AddressBuilder;
 import it.university.department.entity.builder.Builder;
 import it.university.department.entity.builder.FacultyBuilder;
+import it.university.department.entity.builder.StudentBuilder;
 
 @Configuration
 public class BuilderConfiguration {
@@ -20,6 +22,11 @@ public class BuilderConfiguration {
 	@Bean
 	public Builder<Faculty> getFacultyBuilder() {
 		return new FacultyBuilder();
+	}
+	
+	@Bean
+	public Builder<Student> getStudentBuilder() {
+		return new StudentBuilder();
 	}
 	
 }
