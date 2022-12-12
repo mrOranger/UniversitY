386c50d655cc0cbfbc12617bd098342cc9926930
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-12 16:16:41 +0100

    Add Bean for Department Builder

--- university-department/src/main/java/it/university/department/BuilderConfiguration.java
+++ university-department/src/main/java/it/university/department/BuilderConfiguration.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import it.university.department.entity.Address;
+import it.university.department.entity.Department;
 import it.university.department.entity.Faculty;
 import it.university.department.entity.Student;
 import it.university.department.entity.builder.AddressBuilder;
 import it.university.department.entity.builder.Builder;
+import it.university.department.entity.builder.DepartmentBuilder;
 import it.university.department.entity.builder.FacultyBuilder;
 import it.university.department.entity.builder.StudentBuilder;
 
@@ -27,6 +29,11 @@ public class BuilderConfiguration {
 	@Bean
 	public Builder<Student> getStudentBuilder() {
 		return new StudentBuilder();
+	}
+	
+	@Bean
+	public Builder<Department> getDepartmentBuilder() {
+		return new DepartmentBuilder();
 	}
 	
 }
