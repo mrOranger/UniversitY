ab78d1357ee10ce4e7ba43e442d7d27add5aabcc
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-12 11:36:08 +0100

    Add missing method findByAddressStreet

--- university-department/src/main/java/it/university/department/repository/DepartmentRepository.java
+++ university-department/src/main/java/it/university/department/repository/DepartmentRepository.java
@@ -8,6 +8,7 @@ import it.university.department.entity.Department;
 
 public interface DepartmentRepository extends JpaRepository<Department, String>{
 	
+	public abstract List<Department> findByAddressStreet(String street);
 	public abstract List<Department> findByAddressCity(String city);
 	public abstract List<Department> findByAddressProvince(String province);
 	public abstract List<Department> findByAddressRegion(String region);
