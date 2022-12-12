4a73a25b6a565f5c435bef532176b1801e9d9912
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-12 11:28:40 +0100

    Complete repository for Student

--- university-department/src/main/java/it/university/department/repository/StudentRepository.java
+++ university-department/src/main/java/it/university/department/repository/StudentRepository.java
@@ -1,9 +1,16 @@
 package it.university.department.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import it.university.department.entity.Student;
 
 public interface StudentRepository extends JpaRepository<Student, String>{
-
+	
+	@Query(value = "SELECT S.* FROM student S, join_department jd WHERE S.id = jd.student AND jd.department = :department")
+	public List<Student> findAllByDepartment(@Param("department") String department);
+	
 }
