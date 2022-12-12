9f993def1b58b058d9686ceac914675623a1709a
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-12 10:04:46 +0100

    Complete definition of entities

---
+++ university-department/src/main/java/it/university/department/entity/Department.java
@@ -0,0 +1,53 @@
+package it.university.department.entity;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity @Table(name = "department")
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Department implements Serializable {
+
+	private static final long serialVersionUID = 4255798146306630879L;
+	
+	@Id @Column(name = "name")
+	private String name;
+	
+	@JsonBackReference
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "address", referencedColumnName = "id")
+	@EqualsAndHashCode.Exclude
+	private Address address;
+	
+	@JsonBackReference
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "faculty", referencedColumnName = "name")
+	@EqualsAndHashCode.Exclude
+	private Faculty faculty;
+	
+	@JsonManagedReference
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "worksIn")
+	private Set<Professor> professors = new HashSet<>();
+
+	@JsonManagedReference
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "director")
+	private Set<Professor> directors = new HashSet<>();
+}
---
+++ university-department/src/main/java/it/university/department/entity/Faculty.java
@@ -0,0 +1,43 @@
+package it.university.department.entity;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity @Table(name = "faculty") 
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Faculty implements Serializable {
+
+	private static final long serialVersionUID = -497193358975253887L;
+	
+	@Id @Column(name = "name")
+	private String name;
+	
+	@JsonBackReference
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "address", referencedColumnName = "id")
+	@EqualsAndHashCode.Exclude
+	private Address address;
+	
+	@JsonManagedReference
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "faculty", orphanRemoval = true)
+	private Set<Department> departments = new HashSet<>();
+}
