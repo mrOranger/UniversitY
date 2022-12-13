c81d6180320e8ac28112867f4200ab50b7914a1c
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-13 12:00:57 +0100

    Fix bug for professor findByWorksInName method

--- university-department/src/main/java/it/university/department/repository/ProfessorRepository.java
+++ university-department/src/main/java/it/university/department/repository/ProfessorRepository.java
@@ -4,15 +4,16 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import it.university.department.entity.Professor;
 
 public interface ProfessorRepository extends JpaRepository<Professor, String>{
 
-	@Query(value = "SELECT * FROM professor WHERE department = :1", nativeQuery = true)
-	public abstract List<Professor> findByWorksInName(String department);
+	@Query(value = "SELECT * FROM professor WHERE department = :department", nativeQuery = true)
+	public abstract List<Professor> findByWorksInName(@Param("department") String department);
 	
-	@Query(value = "SELECT * FROM professor WHERE director = :1", nativeQuery = true)
-	public abstract List<Professor> findByDirectorName(String department);
+	@Query(value = "SELECT * FROM professor WHERE director = :department", nativeQuery = true)
+	public abstract List<Professor> findByDirectorName(@Param("department") String department);
 	
 }
