0133ef064430d6d2377d225ec3e04e72998a2985
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-13 12:57:27 +0100

    Add OpenApi configuration class for Swagger

---
+++ university-department/src/main/java/it/university/department/OpenApiConfiguration.java
@@ -0,0 +1,27 @@
+package it.university.department;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public final class OpenApiConfiguration {
+
+	@Bean
+	public OpenAPI getCustomOpenApi() {
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info()
+						.title("Progetto UniversitY - Dipartimenti Web Service")
+						.description("REST API in Spring Boot per la gestione dei Dipartimenti, del progetto UniversitY")
+						.contact(new Contact().email("edo.oranger@email.com").name("Edoardo Oranger").url("https://github.com/mrOranger/"))
+						.license(new License().name("GNU General Public License v3.0").url("https://www.gnu.org/licenses/gpl-3.0.en.html"))
+						.version("1.0"));
+	}
+	
+}
