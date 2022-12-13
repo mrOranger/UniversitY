5daf9ce256f8d10fd9c3501e3029dac0516db3c6
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-13 18:40:35 +0100

    Fix bug from reference to AddressDTO

--- university-department/src/main/java/it/university/department/dto/DepartmentDTO.java
+++ university-department/src/main/java/it/university/department/dto/DepartmentDTO.java
@@ -1,7 +1,5 @@
 package it.university.department.dto;
 
-import it.university.department.entity.Address;
-import it.university.department.entity.Faculty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +8,7 @@ import lombok.NoArgsConstructor;
 public final class DepartmentDTO {
 	
 	private String name;
-	private Address address;
-	private Faculty faculty;
+	private AddressDTO address;
+	private FacultyDTO faculty;
 	
 }
