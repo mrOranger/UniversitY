62096b134c6d8f514cf940a4732a29eba2af71f1
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-13 11:57:12 +0100

    Modified native queries

--- university-department/src/main/java/it/university/department/repository/ProfessorRepository.java
+++ university-department/src/main/java/it/university/department/repository/ProfessorRepository.java
@@ -3,12 +3,16 @@ package it.university.department.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import it.university.department.entity.Professor;
 
 public interface ProfessorRepository extends JpaRepository<Professor, String>{
 
+	@Query(value = "SELECT * FROM professor WHERE department = :1", nativeQuery = true)
 	public abstract List<Professor> findByWorksInName(String department);
+	
+	@Query(value = "SELECT * FROM professor WHERE director = :1", nativeQuery = true)
 	public abstract List<Professor> findByDirectorName(String department);
 	
 }
