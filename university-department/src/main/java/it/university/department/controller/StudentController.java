1c9b407e675c5c11c92b3d7b5808ce624772620b
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-14 17:32:03 +0100

    Removed Update tests for Student

--- university-department/src/main/java/it/university/department/controller/StudentController.java
+++ university-department/src/main/java/it/university/department/controller/StudentController.java
@@ -4,7 +4,7 @@ package it.university.department.controller;
 /*
  * [GET] -> api/departments/students/:department -> [Students]
  * [POST] -> api/department/students -> Message
- * [Delete] -> api/departments/students/:studentId -> Message
+ * [DELETE] -> api/departments/students/:studentId -> Message
  */
 public final class StudentController {
 
