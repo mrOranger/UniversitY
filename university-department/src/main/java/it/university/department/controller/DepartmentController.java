f67422f716a75abd4788e6107bbe82bd442fdf93
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-14 10:59:28 +0100

    Setup routes tasks for department

--- university-department/src/main/java/it/university/department/controller/DepartmentController.java
+++ university-department/src/main/java/it/university/department/controller/DepartmentController.java
@@ -1,6 +1,29 @@
 package it.university.department.controller;
 
 // TODO: complete definition of controller for departments
+/*
+ * GET -> api/departments/ -> [Department]
+ * GET -> api/departments/:name -> Department
+ * POST -> api/departments -> Department
+ * PUT -> api/departments/:name -> Department
+ * PATCH -> api/departments/:oldName/name/:newName -> Department
+ * DELETE -> api/departments/:name -> Department
+ *  {
+ *  	name: string,
+ *  	address: {
+ *  		id: integer,
+ *  		street: string,
+ *  		number: integer,
+ *  		city: string,
+ *  		province: string,
+ *  		region: string,
+ *  		nation: string,
+ *  	},
+ *  	faculty: {
+ *  		name: string
+ *  	}
+ *  }
+ */
 public final class DepartmentController {
 
 }
