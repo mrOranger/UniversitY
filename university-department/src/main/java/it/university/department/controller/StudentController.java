2c8f973a4ce1d2a00ae7e5bf5da26093ccd2f712
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-14 18:11:46 +0100

    Complete Student controlelr

--- university-department/src/main/java/it/university/department/controller/StudentController.java
+++ university-department/src/main/java/it/university/department/controller/StudentController.java
@@ -3,6 +3,7 @@ package it.university.department.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,23 +11,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.university.department.dao.impl.DepartmentService;
 import it.university.department.dao.impl.StudentService;
 import it.university.department.dto.StudentDTO;
 import it.university.department.exception.NotFoundException;
 import lombok.SneakyThrows;
 import lombok.extern.java.Log;
 
-// TODO: complete definition of controller for students
-/*
- * [GET] -> api/departments/students/:department -> [Students]
- * [POST] -> api/department/students -> Message
- * [DELETE] -> api/departments/students/:studentId -> Message
- */
 @RestController @Log
 @RequestMapping(path = "api/departments/students")
 public final class StudentController {
 	
+	@Autowired private DepartmentService departmentService;
 	@Autowired private StudentService studentService;
+	@Autowired private ResourceBundleMessageSource errorMessage;
 	
 	@GetMapping(path = "/{department}") @SneakyThrows
 	public final ResponseEntity<List<StudentDTO>> getStudents(@PathVariable("department") String department) {
@@ -37,5 +35,4 @@ public final class StudentController {
 		}
 		return new ResponseEntity<List<StudentDTO>>(students, HttpStatus.OK);
 	}
-
 }
