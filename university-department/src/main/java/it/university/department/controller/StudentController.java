d04d8b4801069cb5ad075c73512e4e2c88cc1b5a
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-15 09:26:29 +0100

    Removed not necessary test classes

--- university-department/src/main/java/it/university/department/controller/StudentController.java
+++ university-department/src/main/java/it/university/department/controller/StudentController.java
@@ -3,7 +3,6 @@ package it.university.department.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import it.university.department.dao.impl.DepartmentService;
 import it.university.department.dao.impl.StudentService;
 import it.university.department.dto.StudentDTO;
 import it.university.department.exception.NotFoundException;
@@ -22,9 +20,7 @@ import lombok.extern.java.Log;
 @RequestMapping(path = "api/departments/students")
 public final class StudentController {
 	
-	@Autowired private DepartmentService departmentService;
 	@Autowired private StudentService studentService;
-	@Autowired private ResourceBundleMessageSource errorMessage;
 	
 	@GetMapping(path = "/{department}") @SneakyThrows
 	public final ResponseEntity<List<StudentDTO>> getStudents(@PathVariable("department") String department) {
