68e9f31fe72d6878e15a355a35a9c77ddcb2f2ec
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-15 13:43:35 +0100

    Setup of cache configuration class

---
+++ university-department/src/main/java/it/university/department/cache/CacheConfiguration.java
@@ -0,0 +1,9 @@
+package it.university.department.cache;
+
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration @CacheConfig
+public class CacheConfiguration {
+
+}
