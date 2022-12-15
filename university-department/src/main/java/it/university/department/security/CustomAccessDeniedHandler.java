1ba74fb43c68cd9b85cac2fb21add9f1b6c0d9f9
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-15 16:49:27 +0100

    Complete configuration of KeyCloak

---
+++ university-department/src/main/java/it/university/department/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,24 @@
+package it.university.department.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+			org.springframework.security.access.AccessDeniedException accessDeniedException)
+			throws IOException, ServletException {
+		final String errorMessage = "Privilegi Insufficienti. Impossibile Proseguire";
+		HttpStatus httpStatus = HttpStatus.FORBIDDEN;
+		response.setStatus(httpStatus.value());
+		response.getOutputStream().println(errorMessage);
+	}
+
+}
