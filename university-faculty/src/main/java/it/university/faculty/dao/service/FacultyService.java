627b39eba959b44d43cef351aaf11b803cbf331c
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-16 17:38:23 +0100

    Created service class for Faculty

---
+++ university-faculty/src/main/java/it/university/faculty/dao/service/FacultyService.java
@@ -0,0 +1,46 @@
+package it.university.faculty.dao.service;
+
+import java.util.List;
+
+import it.university.faculty.dao.FacultyDAO;
+import it.university.faculty.dto.Faculty;
+
+public class FacultyService implements FacultyDAO {
+
+	@Override
+	public List<Faculty> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Faculty findById(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Faculty> findAllByCity(String city) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Faculty> findAllByProvince(String province) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Faculty> findAllByRegion(String region) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Faculty> findAllByNation(String nation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
