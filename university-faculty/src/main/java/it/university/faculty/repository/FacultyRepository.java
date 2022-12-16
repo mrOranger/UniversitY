1b7d7d30227de36ab49c89be3e8180d9c5d8a4fd
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-16 17:41:47 +0100

    Add methods to JPARepository for Faculty

--- university-faculty/src/main/java/it/university/faculty/repository/FacultyRepository.java
+++ university-faculty/src/main/java/it/university/faculty/repository/FacultyRepository.java
@@ -1,9 +1,16 @@
 package it.university.faculty.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import it.university.faculty.dto.Faculty;
+import it.university.faculty.entity.FacultyEntity;
 
-public interface FacultyRepository extends JpaRepository<Faculty, String> {
+public interface FacultyRepository extends JpaRepository<FacultyEntity, String> {
 
+	public abstract List<FacultyEntity> findByAddressCity(String city);
+	public abstract List<FacultyEntity> findByAddressProvince(String province);
+	public abstract List<FacultyEntity> findByAddressRegion(String region);
+	public abstract List<FacultyEntity> findByAddressNation(String nation);
+	
 }
