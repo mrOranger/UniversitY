61ba26709470b1d8755cc2f0e887fd8abcab71a5
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-16 17:54:57 +0100

    Complete implementation of test methods

--- university-faculty/src/test/java/it/university/faculty/respository/faculty/TestSelectFaculty.java
+++ university-faculty/src/test/java/it/university/faculty/respository/faculty/TestSelectFaculty.java
@@ -3,14 +3,15 @@ package it.university.faculty.respository.faculty;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-import java.util.List;
-
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import it.university.faculty.dao.service.FacultyService;
+import it.university.faculty.dto.Faculty;
 
 @SpringBootTest @TestMethodOrder(OrderAnnotation.class) @Order(1)
 public final class TestSelectFaculty {
@@ -18,70 +19,68 @@ public final class TestSelectFaculty {
 	@Autowired private FacultyService facultyService;
 	
 	@Test @Order(1)
-	public final void testSelectFaculty() {
-		assertThat(this.facultyService.findAll())
-			.extracting(List::size)
+	public final void testSelectFaculties() {
+		assertThat(this.facultyService.findAll().size())
 			.isEqualTo(3);
 	}
 	
 	@Test @Order(2)
+	public final void testSelectFaculty() {
+		assertThat(this.facultyService.findById("Ingegneria"))
+			.extracting(Faculty::getName)
+			.isEqualTo("Ingegneria");
+	}
+	
+	@Test @Order(3)
 	public final void testSelectFacultyNotFound() {
 		assertNull(this.facultyService.findById("Medicina"));
 	}
 	
-	@Test @Order(3)
+	@Test @Order(4)
 	public final void testSelectFacultyByAddressCity() {
-		assertThat(this.facultyService.findAllByCity("Milano"))
-			.extracting(List::size)
+		assertThat(this.facultyService.findAllByCity("Milano").size())
 			.isEqualTo(3);
 	}
 	
-	@Test @Order(4)
+	@Test @Order(5)
 	public final void testSelectFacultyByAddressCityNotFound() {
-		assertThat(this.facultyService.findAllByCity("Napoli"))
-			.extracting(List::size)
+		assertThat(this.facultyService.findAllByCity("Napoli").size())
 			.isEqualTo(0);
 	}
 	
-	@Test @Order(5)
+	@Test @Order(6)
 	public final void testSelectFacultyByAddressProvince() {
-		assertThat(this.facultyService.findAllByProvince("Milano"))
-			.extracting(List::size)
+		assertThat(this.facultyService.findAllByProvince("Milano").size())
 			.isEqualTo(2);
 	}
 	
-	@Test @Order(6)
-	public final void testSelectFacultyByAddressProvinceNotFound() {
-		assertThat(this.facultyService.findAllByProvince("Napoli"))
-			.extracting(List::size)
-			.isEqualTo(0);		
-	}
-	
 	@Test @Order(7)
-	public final void testSelectFacultyByAddressRegion() {
-		assertThat(this.facultyService.findAllByRegion("Lombardia"))
-			.extracting(List::size)
-			.isEqualTo(3);		
+	public final void testSelectFacultyByAddressProvinceNotFound() {
+		assertThat(this.facultyService.findAllByProvince("Napoli").size())
+			.isEqualTo(0);		
 	}
 	
 	@Test @Order(8)
-	public final void testSelectFacultyByAddressRegionNotFound() {
-		assertThat(this.facultyService.findAllByRegion("Campania"))
-			.extracting(List::size)
-			.isEqualTo(0);		
-	}
-	
-	@Test @Order(9)
-	public final void testSelectFacultyByAddressNation() {
-		assertThat(this.facultyService.findAllByNation("Italia"))
-			.extracting(List::size)
+	public final void testSelectFacultyByAddressRegion() {
+		assertThat(this.facultyService.findAllByRegion("Lombardia").size())
 			.isEqualTo(3);		
 	}
 	
+	@Test @Order(9)
+	public final void testSelectFacultyByAddressRegionNotFound() {
+		assertThat(this.facultyService.findAllByRegion("Campania").size())
+			.isEqualTo(0);		
+	}
+	
 	@Test @Order(10)
+	public final void testSelectFacultyByAddressNation() {
+		assertThat(this.facultyService.findAllByNation("Italia").size())
+			.isEqualTo(3);		
+	}
+	
+	@Test @Order(11)
 	public final void testSelectFacultyByAddressNationNotFound() {
-		assertThat(this.facultyService.findAllByNation("USA"))
-			.extracting(List::size)
+		assertThat(this.facultyService.findAllByNation("USA").size())
 			.isEqualTo(0);		
 	}
 }
