0ee55831bca742cbe49db36ac10b4ded9abcf8ad
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-16 12:00:33 +0100

    Create basic classes for project

---
+++ university-faculty/src/main/java/it/university/faculty/UniversityFacultyApplication.java
@@ -0,0 +1,13 @@
+package it.university.faculty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityFacultyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityFacultyApplication.class, args);
+	}
+
+}
