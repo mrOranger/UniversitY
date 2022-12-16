4606af343dd3bf70cf6e41a1614d9be59c5c7e6a
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-16 17:18:46 +0100

    Created entity classes for Address and Faculty

---
+++ university-faculty/src/main/java/it/university/faculty/entity/AddressEntity.java
@@ -0,0 +1,64 @@
+package it.university.faculty.entity;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Table(name = "address")
+@Data @AllArgsConstructor @NoArgsConstructor
+public final class AddressEntity implements Serializable {
+	
+	private static final long serialVersionUID = -146115546931957518L;
+
+	@Id 
+	@Column(name = "id")
+	@NotNull(message = "{NotNull.Address.Id.Validation}")
+	private int id;
+	
+	@Column(name = "street")
+	@Size(max = 50, message = "Size.Address.Street.Validation")
+	@NotNull(message = "{NotNull.Address.Street.Validation}")
+	private String street;
+
+	@Column(name = "number")
+	@NotNull(message = "{NotNull.Address.Number.Validation}")
+	private int number;
+	
+	@Column(name = "city")
+	@NotNull(message = "{NotNull.Address.City.Validation}")
+	@Size(max = 50, message = "{Size.Address.City.Validation}")
+	private String city;
+	
+	@Column(name = "province")
+	@Size(max = 50, message = "{Size.Address.Province.Validation}")
+	private String province;
+	
+	@Column(name = "region")
+	@Size(max = 50, message = "{Size.Address.Region.Validation}")
+	private String region;
+	
+	@Column(name = "nation")
+	@Size(max = 50, message = "{Size.Address.Nation.Validation}")
+	@NotNull(message = "{NotNull.Address.Nation.Validation}")
+	private String nation;
+	
+	@JsonManagedReference
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "address", orphanRemoval = true)
+	private Set<FacultyEntity> departments = new HashSet<>();
+	
+}
---
+++ university-faculty/src/main/java/it/university/faculty/entity/FacultyEntity.java
@@ -0,0 +1,35 @@
+package it.university.faculty.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Table(name = "faculty") 
+@Data @AllArgsConstructor @NoArgsConstructor
+public final class FacultyEntity implements Serializable {
+
+	private static final long serialVersionUID = 2429376661192847168L;
+
+	@Id @Column(name = "name")
+	@NotNull(message = "{NotNull.Faculty.Name.Validation}")
+	private String name;
+	
+	@JsonBackReference
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "address", referencedColumnName = "id")
+	private AddressEntity address;
+}
