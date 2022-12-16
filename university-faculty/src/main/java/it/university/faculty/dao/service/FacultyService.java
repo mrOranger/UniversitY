652a160d76c6023b8d69424e61b44264be0e3578
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-12-16 17:46:24 +0100

    Implemented methods in service class

--- university-faculty/src/main/java/it/university/faculty/dao/service/FacultyService.java
+++ university-faculty/src/main/java/it/university/faculty/dao/service/FacultyService.java
@@ -1,46 +1,54 @@
 package it.university.faculty.dao.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import it.university.faculty.dao.FacultyDAO;
 import it.university.faculty.dto.Faculty;
+import it.university.faculty.entity.FacultyEntity;
+import it.university.faculty.repository.FacultyRepository;
 
 public class FacultyService implements FacultyDAO {
+	
+	@Autowired private FacultyRepository facultyRepository;
 
 	@Override
 	public List<Faculty> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.convertToDto(this.facultyRepository.findAll());
 	}
 
 	@Override
 	public Faculty findById(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.convertToDto(this.facultyRepository.findById(name));
 	}
 
 	@Override
 	public List<Faculty> findAllByCity(String city) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.convertToDto(this.facultyRepository.findByAddressCity(city));
 	}
 
 	@Override
 	public List<Faculty> findAllByProvince(String province) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.convertToDto(this.facultyRepository.findByAddressProvince(province));
 	}
 
 	@Override
 	public List<Faculty> findAllByRegion(String region) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.convertToDto(this.facultyRepository.findByAddressRegion(region));
 	}
 
 	@Override
 	public List<Faculty> findAllByNation(String nation) {
-		// TODO Auto-generated method stub
+		return this.convertToDto(this.facultyRepository.findByAddressNation(nation));
+	}
+	
+	private Faculty convertToDto(Optional<FacultyEntity> faculty) {
 		return null;
 	}
-
+	
+	private List<Faculty> convertToDto(List<FacultyEntity> faculties){
+		return null;
+	}
 }
